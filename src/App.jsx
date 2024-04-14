202204a227e462d2be40a8e52e239e19c2d8e216
ManupaDev <manupadevsuite@gmail.com>
2024-04-14 16:04:09 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button/Button.jsx";
+
+function App() {
+  return (
+    <main className="p-4">
+      <Button />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Button/Button.jsx
@@ -0,0 +1,5 @@
+function Button() {
+  return <button className="px-4 py-2 rounded text-white bg-blue-500 font-semibold">Click Me</button>;
+}
+
+export default Button;
