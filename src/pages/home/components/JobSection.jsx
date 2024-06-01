87f7c4a9ef74182c45367d27ef651541067c08e8
ManupaDev <manupadevsuite@gmail.com>
2024-06-01 22:49:50 +0530

    feat: data fethcing and mutations

--- src/pages/home/components/JobSection.jsx
+++ src/pages/home/components/JobSection.jsx
@@ -1,26 +1,54 @@
 import JobCard from "@/components/shared/JobCard";
+import { useEffect, useState } from "react";
+
+const getJobs = async () => {
+  const res = await fetch("http://localhost:8000/jobs", {
+    method: "GET",
+  });
+  const jobs = await res.json();
+  return jobs;
+};
 
 function JobSection() {
-  const jobs = [
-    {
-      _id: "xyz",
-      title: "Intern - Software Engineer",
-      type: "Full-time",
-      location: "Remote",
-    },
-    {
-      _id: "abc",
-      title: "Software Engineer",
-      type: "Full-time",
-      location: "Colombo, Sri Lanka",
-    },
-    {
-      _id: "abc",
-      title: "Software Architect",
-      type: "Hybrid",
-      location: "Rajagiriya, Sri Lanka",
-    },
-  ];
+  const [jobs, setJobs] = useState([]);
+  const [isJobsLoading, setIsJobsLoading] = useState(false);
+  const [isJobsError, setIsJobsError] = useState(false);
+
+  useEffect(() => {
+    setIsJobsLoading(true);
+    getJobs()
+      .then((data) => {
+        setJobs(data);
+      })
+      .catch((err) => {
+        setIsJobsError(true);
+      })
+      .finally(() => {
+        setIsJobsLoading(false);
+      });
+  }, []);
+
+  if (isJobsLoading) {
+    return (
+      <section className="py-8">
+        <h2>Available Jobs</h2>
+        <div className="mt-4 flex flex-col gap-y-8">
+          <p>Loading...</p>
+        </div>
+      </section>
+    );
+  }
+
+  if (isJobsError) {
+    return (
+      <section className="py-8">
+        <h2>Available Jobs</h2>
+        <div className="mt-4 flex flex-col gap-y-8">
+          <p>Error while fething data...</p>
+        </div>
+      </section>
+    );
+  }
 
   return (
     <section className="py-8">
