02bb338dffa9941d89052794b19fade830f1aa83
ManupaDev <manuapdevsuite@gmail.com>
2024-08-10 14:17:36 +0530

    fix: tsx to jsx

--- src/pages/admin/job/components/JobApplicationCard.jsx
+++ src/pages/admin/job/components/JobApplicationCard.jsx
@@ -2,13 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Link } from "react-router-dom";
 
-type JobApplicationCardProps = {
-  _id: string;
-  jobId: string;
-  fullName: string;
-};
-
-function JobApplicationCard({ _id, jobId, fullName }: JobApplicationCardProps) {
+function JobApplicationCard({ _id, jobId, fullName }) {
   return (
     <Link to={`/admin/job/${jobId}/application/${_id}`} className="block">
       <Card>
